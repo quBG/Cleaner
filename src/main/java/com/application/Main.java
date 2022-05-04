4e931acbf84a222b9c343b3a9fd37eff75ed432a
QUBGames <johnivanhovich@gmail.com>
2022-05-04 19:16:06 +0300

    putting the project in order

--- src/main/java/com/application/Main.java
+++ src/main/java/com/application/Main.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-public class MainApplication extends Application {
+public class Main extends Application {
 
     public static void main(String[] args) {
         launch();
@@ -14,11 +14,12 @@ public class MainApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("MainApplication.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainApplication.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setMinHeight(450);
-        stage.setMinWidth(700);
-        stage.setMaximized(true);
+        stage.setMinHeight(555);
+        stage.setMinWidth(899);
+        stage.setMaxHeight(556);
+        stage.setMaxWidth(900);
         stage.setTitle("Cleaner");
         stage.setScene(scene);
         stage.centerOnScreen();
