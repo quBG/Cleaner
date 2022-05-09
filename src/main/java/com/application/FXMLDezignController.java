c1583722344d032e576f9e9d262f58e39eb78025
QUBGames <johnivanhovich@gmail.com>
2022-05-09 16:45:18 +0300

    work done

--- src/main/java/com/application/FXMLDezignController.java
+++ src/main/java/com/application/FXMLDezignController.java
@@ -1,9 +1,9 @@
 package com.application;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
@@ -12,19 +12,16 @@ import java.util.*;
 public class FXMLDezignController {
 
     @FXML
-    public VBox containerButtons;
-    @FXML
     public VBox containerPictures;
+    @FXML
+    public ListView modeSelection;
 
-    private List<Button> selectingModes = new ArrayList<>();
     private List<ImageView> modePictures = new ArrayList<>();
 
     @FXML
     public void initialize() {
-        VboxToList(containerButtons, selectingModes);
         VboxToList(containerPictures, modePictures);
 
-        containerButtons.getOnMouseClicked();
     }
 
     private <T> void VboxToList(VBox container, List<T> list) {
