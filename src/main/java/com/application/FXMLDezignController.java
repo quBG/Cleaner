4da035398be38d00eefd5da3666de055e583efa5
QUBGames <johnivanhovich@gmail.com>
2022-05-10 09:30:28 +0300

    work done

--- src/main/java/com/application/FXMLDezignController.java
+++ src/main/java/com/application/FXMLDezignController.java
@@ -1,31 +1,37 @@
 package com.application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
-
 import java.util.*;
 
 public class FXMLDezignController {
 
     @FXML
-    public VBox containerPictures;
+    private VBox containerButtons;
     @FXML
-    public ListView modeSelection;
+    private BorderPane borderPane;
 
-    private List<ImageView> modePictures = new ArrayList<>();
+    private List<Button> modeSelections = new ArrayList<>();
 
     @FXML
     public void initialize() {
-        VboxToList(containerPictures, modePictures);
-
+        VboxToList(containerButtons, modeSelections);
     }
 
     private <T> void VboxToList(VBox container, List<T> list) {
             for (Node element : container.getChildren().stream().toList())
                 list.add((T)element);
+    }
+
+    public void SetPage(ActionEvent actionEvent) {
+
     }
 }
