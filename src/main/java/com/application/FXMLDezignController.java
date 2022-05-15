3ef055bb54ea836883880eec4efad5a5bfc6c150
QUBGames <johnivanhovich@gmail.com>
2022-05-15 18:31:38 +0300

    Fixed all bugs

--- src/main/java/com/application/FXMLDezignController.java
+++ src/main/java/com/application/FXMLDezignController.java
@@ -5,8 +5,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.effect.Bloom;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import java.net.URL;
 import java.util.*;
 
 public class FXMLDezignController {
@@ -24,14 +26,33 @@ public class FXMLDezignController {
     }
 
     public void SetPage(ActionEvent e) {
-        try{
-            String view = (String) ((Node)e.getSource()).getUserData();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(view));
+        String view = (String) ((Node) e.getSource()).getUserData();
+        loadFXML(getClass().getResource(view));
+        Button button = (Button) e.getSource();
+        ButtonBacklight(button);
+    }
+
+    private void loadFXML(URL path){
+        try {
+            FXMLLoader loader = new FXMLLoader(path);
             borderPane.setCenter(loader.load());
-        }catch (Exception exception){
-            System.out.println(exception);
+        }
+        catch (Exception e) {
+            System.out.println(e);
         }
     }
+
+    private void ButtonBacklight(Button button){
+        ButtonClening();
+        button.setEffect(new Bloom());
+    }
+
+    private void ButtonClening(){
+        for (Button button : modeSelections) {
+            button.setEffect(null);
+        }
+    }
+
 
     private <T> void VboxToList(VBox container, List<T> list) {
             for (Node element : container.getChildren().stream().toList())
