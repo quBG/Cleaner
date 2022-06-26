e04ccf949d1210f73e2baae882f607b5f2b83b38
QUBGames <johnivanhovich@gmail.com>
2022-06-26 12:47:01 +0300

    trying to combine c and java

--- src/main/java/com/application/pages/ClearingMemory.java
+++ src/main/java/com/application/pages/ClearingMemory.java
@@ -1,10 +1,12 @@
 package com.application.pages;
 
+import com.application.tools.memory.Cleaning;
 import javafx.event.ActionEvent;
 
-public class ClearingMemory extends Page{
+public class ClearingMemory{
 
     public void clear(ActionEvent event) {
+        Cleaning.use();
         System.out.println("Complete Cleaning Memory");
     }
 }
---
+++ src/main/java/com/application/tools/memory/Cleaning.java
@@ -0,0 +1,5 @@
+package com.application.tools.memory;
+
+public class Cleaning {
+    public static native void use();
+}
