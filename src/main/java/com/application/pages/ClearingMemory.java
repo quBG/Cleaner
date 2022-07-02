ded4edf11ee52f941e92e703e7ccc9b397103988
QUBGames <johnivanhovich@gmail.com>
2022-07-02 19:46:37 +0300

    added memory cleanup service, successful

--- src/main/java/com/application/pages/ClearingMemory.java
+++ src/main/java/com/application/pages/ClearingMemory.java
@@ -1,9 +1,29 @@
 package com.application.pages;
 
 import javafx.event.ActionEvent;
+import java.awt.Desktop;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class ClearingMemory{
     public void clear(ActionEvent event) {
+        useCleaningService();
         System.out.println("Complete Cleaning Memory");
+    }
+
+    private void useCleaningService() {
+        Path exe = Path.of(System.getProperty("user.home"), ".ACCleaner", "SetProcessWorkingSetSize.exe");
+        try {
+            if (Files.notExists(exe)) {
+                try (InputStream in = getClass().getResourceAsStream("/com/application/exeServices/SetProcessWorkingSetSize.exe")) {
+                    Files.createDirectories(exe.getParent());
+                    Files.copy(in, exe);
+                }
+            }
+            Desktop.getDesktop().open(exe.toFile());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.cleaner.main.cleaner {
     requires javafx.fxml;
     requires javafx.controls;
-
     requires java.logging;
+    requires java.desktop;
 
     opens com.application to javafx.fxml;
     exports com.application.pages;
