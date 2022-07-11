4d09649b5bd41bcaac5e72968d404fd1e7ed62cf
QUBGames <johnivanhovich@gmail.com>
2022-07-11 12:42:37 +0300

    finished clearing files

--- src/main/java/application/pages/CleaningFiles.java
+++ src/main/java/application/pages/CleaningFiles.java
@@ -2,10 +2,18 @@ package application.pages;
 
 import application.tools.ExeTools;
 import javafx.event.ActionEvent;
+import javafx.scene.layout.HBox;
 
 public class CleaningFiles {
+    public HBox cleaningButton;
+    public HBox background;
+    public HBox cleaningCompletionBackground;
+
     public void clear(ActionEvent event) {
+        cleaningButton.setVisible(false);
+        background.setVisible(false);
         ExeTools.startExeService("DiskSpaceCleanup.exe",
                 "/application/exeServices/DiskSpaceCleanup.exe");
+        cleaningCompletionBackground.setVisible(true);
     }
 }
