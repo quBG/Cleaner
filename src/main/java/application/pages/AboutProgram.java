1d1ce6cb5672d98458141b53e94f6014c66dd1d3
QUBGames <johnivanhovich@gmail.com>
2022-08-10 16:24:13 +0300

    work done

--- src/main/java/application/pages/AboutProgram.java
+++ src/main/java/application/pages/AboutProgram.java
@@ -1,4 +1,21 @@
 package application.pages;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class AboutProgram {
+    @FXML
+    private void sendToSite(){
+        try {
+            Desktop.getDesktop().browse(new URL("https://github.com/quBG/Cleaner").toURI());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
 }
